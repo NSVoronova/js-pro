32993c706f03de81d5132adc68491011db59bd83
Nadzeya Voranava <nadya.voronova@gmail.com>
2023-08-13 12:50:02 +0200

    init: add hw_37

---
+++ my-app/src/components/Tab/Tab.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface ITab {
+  label: string;
+  isActive: boolean;
+  onClick: () => void;
+}
+const Tab = ({ label, isActive, onClick }: ITab) => (
+  <div
+    className={`tab${isActive ? ' active' : ''}`}
+    onClick={onClick}
+  >
+    {label}
+  </div>
+);
+
+
+export default Tab
---
+++ my-app/src/components/Title/Title.tsx
@@ -0,0 +1,18 @@
+import React, {ReactNode} from 'react';
+import "./style.css";
+
+export interface ITitle {
+  text?: string;
+  children?: ReactNode;
+  customClass?: string;
+
+}
+const Title = ({text}: ITitle) => {
+  return (
+    <h1>
+      {text}
+    </h1>
+  )
+}
+
+export default Title
