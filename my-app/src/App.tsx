70178da9a8ad7ab8dbeb1dffe66dbfec8afcbcb6
Nadzeya Voranava <nadya.voronova@gmail.com>
2023-08-14 12:04:58 +0200

    init: add hw_38

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import PostsList from './components/Posts/PostsList';
 
 function App() {
   return (
     <div className="App">
+      <PostsList/>
     </div>
   );
 }
---
+++ my-app/src/components/Posts/PostsList.tsx
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+import BigPost from './BigPost/BigPost'
+import MiddlePost from './MiddlePost/MiddlePost'
+import LittlePost from './LittlePost/LittlePost'
+import './style.css'
+
+export interface IPost {
+  image: string,
+  text: string,
+  date: string,
+  lesson_num?: number,
+  title: string,
+  author?: number,
+  id: number,
+}
+
+const PostsList = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  let fetchPosts = async () => {
+    try {
+      let responce = await fetch('https://64d916c7e947d30a2609e71e.mockapi.io/posts_cards');
+      let jsonPosts: IPost[] = await responce.json();
+      
+      setPosts(jsonPosts);
+    } catch (err) {
+      alert("LOL");  
+      // здесь пыталась выводить в консоль ошибку, но реакт ругается почему-то...
+    }
+  }
+  return (
+    <>
+      <button onClick={fetchPosts}>
+        Click me
+      </button>
+      <div className='posts__wrapper'>
+        <div className='big-middle__posts'>
+          <div className='big__post'>
+          {posts.map(({id, title, image, text, date}: IPost) => (
+        id === 1  ? (
+          <BigPost key={image} id={id} title={title} text={text} image={image} date={date} />) : <></>))}
+          </div>
+          <div className='middle__posts'>
+          {posts.map(({id, title, image, text, date}: IPost) => (
+        id > 1 && id < 6  ? (
+          <MiddlePost key={image} id={id} title={title} text={text} image={image} date={date} />) : <></>))}
+          </div>
+        </div>
+          <div className='little__posts'>
+          {posts.map(({id, title, image, text, date}: IPost) => (
+        id > 5  ? (
+          <LittlePost key={image} id={id} title={title} text={text} image={image} date={date} />) : <></>))}
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default PostsList
